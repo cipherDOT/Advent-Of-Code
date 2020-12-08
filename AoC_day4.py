85f05318b7fd97cef7661330a53416f6e26d8b20
Kavin Bharathi <66783110+cipherDOT@users.noreply.github.com>
2020-12-08 22:56:13 +0530

    Update AoC_day4.py

--- AoC_day4.py
+++ AoC_day4.py
@@ -32,19 +32,88 @@ with open('input.txt') as f:
             # we take the first three charecter of the elements
             # in a list of elements split by spaces from a stripped line,
             # and add that to the last arr in the data array.
-            data[i] += [i[:3] for i in line.strip().split(' ')]
+            data[i] += [i for i in line.strip().split(' ')]
 
 
-# variables that hold number of valid and
-# non valid passports
+def alphanum(string):
+    total = 0
+    for letter in string:
+        if letter.isdigit() or letter in ['a', 'b', 'c', 'd', 'e', 'f']:
+            total += 1
+
+    if total == len(string):
+        return True
+
+    return False
+
+
+def valid2(arr):
+    count = 0
+    tests = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid']
+
+    for field in arr:
+        key = field[:3]
+        value = field.split(':')[1]
+        # print(key, value)
+        if key in tests:
+            # print(key)
+            if key == 'byr':
+                if 1920 <= int(value) <= 2002:
+                    # print(1920 <= int(value) <= 2002)
+                    count += 1
+
+            elif key == 'iyr':
+                if 2010 <= int(value) <= 2020:
+                    # print(2010 <= int(value) <= 2020)
+                    count += 1
+
+            elif key == 'eyr':
+                if 2020 <= int(value) <= 2030:
+                    # print(2020 <= int(value) <= 2030)
+                    count += 1
+
+            elif key == 'hgt':
+                if value[-2:] == 'cm':
+                    # print(value)
+                    if 150 <= int(value[:-2]) <= 193:
+                        count += 1
+                elif value[-2:] == 'in':
+                    # print(value)
+                    if 59 <= int(value[:-2]) <= 79:
+                        count += 1
+
+            elif key == 'hcl':
+                # print(value)
+                if (value[0] == '#' and alphanum(value[1:])):
+                    # print(value)
+                    count += 1
+
+            elif key == 'ecl':
+                if value in ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']:
+                    # print(value)
+                    count += 1
+
+            elif key == 'pid':
+                if len(str(value)) == 9:
+                    # print(value)
+                    count += 1
+
+    if count == 7:
+        return True
+
+    return False
+
+
 valid_f = 0
 non_valid_f = 0
 
+print(len(data))
 for row in data:
-    if valid(row):
+    if valid2(row):
         valid_f += 1
     else:
         non_valid_f += 1
+
 
 print('valid : ' + str(valid_f))
 print('non valid : ' + str(non_valid_f))
